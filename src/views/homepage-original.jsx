a520de04b9228da76154ec9914106ea53443f6d4
Angelo <angelo@Angelos-MacBook-Pro.local>
2023-06-02 17:21:18 -0400

    app

--- src/views/homepage-original.jsx
+++ src/views/homepage-original.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route } from "react-router-dom"
 import viteLogo from '/vite.svg'
-import "../css/app.css"
+import "../css/App.css"
 import Homepage from '../components/homepage'
 import Homepage2 from '../components/Homepage-Banner2'
 import Homepage3 from '../components/Homepage-Banner3'
